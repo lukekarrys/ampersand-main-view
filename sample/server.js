a7be4dfe07f52afa3339fddfec91659367f902f7
Luke Karrys <lukekarrys@gmail.com>
2016-08-05 23:21:18 -0700

    deps

--- sample/server.js
+++ sample/server.js
@@ -1,6 +1,7 @@
 var Hapi = require('hapi');
-
-var app_config = {
+var MoonbootsHapi = require('moonboots_hapi');
+ 
+var config = {
     appPath: '/{clientPath*}',
     moonboots: {
         main: __dirname + '/client/app.js',
@@ -8,20 +9,16 @@ var app_config = {
         stylesheets: [__dirname + '/public/app.css']
     }
 };
-
-
-var server = new Hapi.Server('0.0.0.0', 3001);
-server.pack.register([{
-    plugin: require('moonboots_hapi'),
-    options: app_config
-}], function startServer(err) {
+ 
+var server = new Hapi.Server();
+server.connection({ host: 'localhost', port: 3001 });
+ 
+server.register({
+    register: MoonbootsHapi,
+    options: config
+}, server.start(function serverStarted(err) {
     if (err) {
         throw err;
     }
-    server.start(function serverStarted(err) {
-        if (err) {
-            throw err;
-        }
-        console.log('App started on localhost:3001');
-    });
-});
+    console.log('App started on localhost:3001');
+}));
